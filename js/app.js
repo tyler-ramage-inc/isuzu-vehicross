06d72a16cf29237e3feb98817931e09810c25599
Tyler Ramage <tysamigo@gmail.com>
2019-03-26 19:08:36 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+// hides the main body content
+const body = document.querySelector('.body');
+body.style.display = 'none';
+
+// click event for body content
+let $whatIsIt = $('#whatIsIt');
+$whatIsIt.click(function(){
+    $('.body').show();
+    $whatIsIt.hide();
+});
